3d9178344f25e26de43b923f3d80755917576334
YangYu <yangyu2010@aliyun.com>
2017-02-14 14:55:42 +0800

    初始化项目

---
+++ DouYuZhiBo/DouYuZhiBo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DouYuZhiBo
+//
+//  Created by Young on 2017/2/14.
+//  Copyright © 2017年 YuYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
